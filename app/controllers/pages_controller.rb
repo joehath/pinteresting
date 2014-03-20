4c1503caa0c289a1fba42d75e5adea1fab8cf201
joehath <joe@joehathaway.com>
2014-03-20 18:51:01 -0400

    configured devise; implemented login/logout users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,9 @@ class PagesController < ApplicationController
   
   def about
   end
+
+  def help
+    # put any code here that you need 
+    # (although for a static view you probably won't have any)
+  end
 end
